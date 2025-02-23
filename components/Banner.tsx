17adc7b690b076fc8f4ddae24798e439e9f7d66b
ahbakken <ahbakken@icloud.com>
2025-02-23 17:13:09 +0100

    banner with photo

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -10,28 +10,31 @@ export default function Banner() {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1.5, ease: "easeOut" }}
-        className="relative z-10 text-center"
+        className="relative z-10 text-center mt-80 md:mt-40"
       >
-        <h1 className="text-6xl font-bold mb-4 text-[#e49133] font-handwriting">
+        <h1 className="text-7xl font-bold mb-4 text-white font-handwriting">
           Edith & Christian
         </h1>
-        <p className="text-2xl mb-8 text-[#003d1e]">skal gifte seg!</p>
+        <p className="text-2xl mb-4 text-white">skal gifte seg!</p>
         <motion.div
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.5, duration: 1 }}
-          className="text-xl font-semibold"
+          className="text-xl font-semibold text-white"
         >
           20. september 2025
         </motion.div>
       </motion.div>
-      <Image
-        src="/placeholder.svg"
-        alt="Edith and Christian"
-        layout="fill"
-        objectFit="cover"
-        className="absolute inset-0 z-0 opacity-30"
-      />
+      <div className="absolute inset-0 z-0 flex items-center justify-center">
+        <Image
+          src="/front-page.jpg"
+          alt="Edith and Christian"
+          priority
+          height={1024}
+          width={700}
+          className="absolute opacity-90"
+        />
+      </div>
       <motion.div
         className="absolute inset-0 z-0"
         initial={{ opacity: 0 }}
