ba40db6c70e41b004b30543b853c5450a312f08f
ahbakken <ahbakken@icloud.com>
2025-03-23 10:49:38 +0100

    rsvp section

--- components/RSVP.tsx
+++ components/RSVP.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { Button } from "./ui/button";
+import { Send } from "lucide-react";
 
 export function RSVP() {
   const handleRSVP = () => {
-    console.log("RSVP");
-    //window.location.href = "https://edithogchristian.no/rsvp";
+    console.log("RSVP to form later");
+    //window.location.href = "det kommer skjemalink her";
   };
   return (
-    <section className="bg-[#850751] h-[50vh] flex flex-col justify-evenly items-center text-center">
+    <section className="bg-[#850751] min-h-[50vh] flex flex-col justify-evenly items-center text-center py-20 gap-12">
       <div className="text-peach font-handwriting text-6xl">RSVP</div>
-      <div className="text-white">
+      <div className="container mx-auto text-white font-bold px-16">
         Skriv inn telefonnummeret ditt for å få tilgang til invitasjonen din og
         gi oss din RSVP. Invitasjonen er knyttet til deg og din familie, og du
         kan oppgi hvor mange som kommer sammen med deg. Klikk på lenken nedenfor
@@ -18,8 +19,13 @@ export function RSVP() {
         svarer så snart som mulig, slik at vi kan forberede oss best mulig for
         alle gjestene.
       </div>
-      <Button className="w-6 bg-[#E49133]" onClick={handleRSVP}>
-        RSVP
+      <Button
+        size="lg"
+        className="mx-auto bg-peach hover:bg-peach text-white  hover:text-[#850751] rounded-[6px] px-16 py-4 gap-3"
+        onClick={handleRSVP}
+      >
+        <Send />
+        <p>Trykk her for å svare i skjemaet</p>
       </Button>
     </section>
   );
